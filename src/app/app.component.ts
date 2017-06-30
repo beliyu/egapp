4834c464f33f60e3b10414dcc20f2b4ea74882e2
Dragan Savic <drsavicrs@gmail.com>
2017-06-30 19:11:27 +0200

    move method to auth.server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AutService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,34 +9,10 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'app works!';
-  user = {group: 0, name: '', id: 0};
-  tok = '';
-constructor( private _ro: Router){}
-  setUser(u) {
-    // We simulate auth servis
-    if (u === 'A') {
-      this.user = {name: 'user A', id: 178, group: 0};
-      this.tok = '123456.798012345.67890123';
-    };
-    if (u === 'B') {
-      this.user = {name: 'user B', id: 251, group: 1};
-      this.tok = '321456.898012345.36890123';
-    };
-    if (u === 'C') {
-      this.user = {name: 'user C', id: 587, group: 2};
-      this.tok = '213456.256012345.15890123';
-    };
-    localStorage.setItem('token', this.tok);
-    localStorage.setItem('user', JSON.stringify(this.user));
-  }
+constructor( private _ro: Router, private _au: AutService) {}
+
   resetUser() {
-    this.user = {group: 0, name: '', id: 0};
-    this.tok = '';
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
+    this._au.resetUser();
     this._ro.navigateByUrl('/');
-  }
-  isTok() {
-    return (this.tok.length > 0) ? true : false;
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,7 +3,42 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class AutService {
+  public user = { group: 0, name: '', id: 0 };
+  public tok = '';
 
+  constructor() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem('token');
+    if (token) {this.tok = token;}
+    if (user) {this.user = user;}
+  }
+
+  public setUser(u) {
+    // We simulate auth servis
+    if (u === 'A') {
+      this.user = { name: 'user A', id: 178, group: 0 };
+      this.tok = '123456.798012345.67890123';
+    };
+    if (u === 'B') {
+      this.user = { name: 'user B', id: 251, group: 1 };
+      this.tok = '321456.898012345.36890123';
+    };
+    if (u === 'C') {
+      this.user = { name: 'user C', id: 587, group: 2 };
+      this.tok = '213456.256012345.15890123';
+    };
+    localStorage.setItem('token', this.tok);
+    localStorage.setItem('user', JSON.stringify(this.user));
+  }
+  public resetUser() {
+    this.user = { group: 0, name: '', id: 0 };
+    this.tok = '';
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+  public isTok() {
+    return (this.tok.length > 0) ? true : false;
+  }
   public getGroup() {
     const user = JSON.parse(localStorage.getItem('user'));
     const group = user && user.group;
